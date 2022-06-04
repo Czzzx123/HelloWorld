3d22e653a1cd8cfe70e39dfb8d67670f5dd40305
czx <729541272@qq.com>
2022-06-04 18:47:10 +0800

    创建main.cpp文件，写了初始代码

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[],int size)
+{
+}
+
+int main()
+{
+	int arr[]={12,23,34,45,56,67};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	return 0;
+}
