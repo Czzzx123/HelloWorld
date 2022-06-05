2777f4027e68df057198e8c79f0c8c5cfc315382
czx <729541272@qq.com>
2022-06-05 17:40:08 +0800

    修改sort为快排

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
 #include<iostream>
 using namespace std;
 
+void sort(int arr[],int i,int j)
+{
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l<r)
+	{
+		while(l < r && arr[r] >= val)
+		{
+			r--;
+		}
+		if(l<r)
+		{
+			arr[l++]=arr[r];
+		}
+		while(l<r&&arr[l]<=val)
+		{
+			l++;
+		}
+		if(l<r)
+		{
+			arr[r--] = arr[l];
+		}
+	}
+	arr[l]=val;
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+}
 void sort(int arr[],int size)
 {
-	int tmp = 0;
-	for(int i=0; i<size-1;++i)
-	{
-        for(int j = 0;j<size-1-i;++j)
-        {
-            if(arr[j] > arr[j+1])
-            {
-                tmp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1]=tmp;
-            }
-        }
-	}
+	sort(arr,0,size);
 }
 
 int main()
