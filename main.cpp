451d193f57b08aee47ed74340e90ea07bb8b9a1e
Czzzx123 <729541272@qq.com>
2022-06-05 13:16:33 +0800

    实现sort 函数

--- main.cpp
+++ main.cpp
@@ -3,12 +3,30 @@ using namespace std;
 
 void sort(int arr[],int size)
 {
+	int tmp = 0;
+	for(int i=0; i<size-1;++i)
+	{
+        for(int j = 0;j<size-1-i;++j)
+        {
+            if(arr[j] > arr[j+1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1]=tmp;
+            }
+        }
+	}
 }
 
 int main()
 {
-	int arr[]={12,23,34,45,56,67};
+	int arr[]={124,486,135,22,23,12,34,45,56,67};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
+    for(auto i:arr)
+    {
+        cout<<i<<' ';
+    }
+    cout<<endl;
 	return 0;
 }
